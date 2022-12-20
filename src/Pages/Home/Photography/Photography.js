1d72346ebc92bbc41841ffe254c5455573737fa1
sabbir1020 <gazisabbir924@gamil.com>
2022-12-20 18:51:46 +0600

    post

--- src/Pages/Home/Photography/Photography.js
+++ src/Pages/Home/Photography/Photography.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaCamera } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import photo from "../../../assets/img/photography.jpg";
 const Photography = () => {
@@ -7,6 +8,7 @@ const Photography = () => {
       <img className=" w-1/2 rounded" src={photo} alt="" />
 
       <div className=" mx-auto my-auto">
+        <FaCamera className="h-10 w-10 mb-4 ml-2"></FaCamera>
         <h1 className="text-2xl mt-1">Modern Photography</h1>
         <p className="mt-2 mb-2">
           This is my photography website. You can hire me.
--- src/Pages/Home/Subscribe/Subscribe.js
+++ src/Pages/Home/Subscribe/Subscribe.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Subscribe = () => {
+  const [subscribe, setSubscribe] = useState(null);
+  const handleSubscribe = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.email.value;
+    console.log(email);
+    fetch("https://photography-server-jade.vercel.app/subscribe", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(subscribe),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // setSubscribe(data);
+        form.reset();
+      });
+  };
   return (
     <div className="mt-4 p-10">
       <h1 className="text-4xl text-center">
-        You will be get Our service <br />
+        Please subscribe to receive our services <br />
         <span className="text-2xl">Please Subscribe</span>
-        <form className="mt-4">
+        <form onSubmit={handleSubscribe} className="mt-4">
           <input
             type="email "
+            name="email"
             placeholder="Your Email"
             className="input input-bordered input-secondary w-full max-w-xs"
           />
